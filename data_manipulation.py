505250c7612e6a17bb4dd2f526b5760d0a0bf200
StevenGaughran <73718182+StevenGaughran@users.noreply.github.com>
2024-01-17 14:11:58 -0800

    Messing about with Pandas and Matplotlib

--- data_manipulation.py
+++ data_manipulation.py
@@ -1,5 +1,8 @@
-import pandas as pd
 import sqlite3
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
 
 def pull_database_data(table=None):
     """Pulls the data from 'TTRPG_rolls.db' and places it in a dataframe.
@@ -13,3 +16,17 @@ def pull_database_data(table=None):
     connect = sqlite3.connect('TTRPG_rolls.db')
     df = pd.read_sql_query(f"SELECT * FROM {table}", connect)
     return df
+
+# def averages_over_games(game_data=pull_database_data('rolls')):
+#     x = game_data['game_number']
+#     y = game_data['roll'].mean()
+#     plt.bar(x,y)
+#     plt.show()
+
+# def averages_over_games(game_data=pull_database_data('rolls')):
+#     x_axis = game_data['game_number']
+#     y_axis = game_data['roll'].mean()
+#     plt.plot(x_axis,y_axis)
+#     plt.show()
+
+# averages_over_games()
