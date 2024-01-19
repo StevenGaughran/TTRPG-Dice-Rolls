e435dd8a1b1623c201ffc40034290f81d0a847a4
StevenGaughran <73718182+StevenGaughran@users.noreply.github.com>
2024-01-19 11:46:29 -0800

    Matplotlib barchart for players' average rolls

--- data_manipulation.py
+++ data_manipulation.py
@@ -17,7 +17,46 @@ def pull_database_data(table=None):
     df = pd.read_sql_query(f"SELECT * FROM {table}", connect)
     return df
 
+def average_rolls_by_player(data=pull_database_data('rolls')):
+    """Creates a barchart of every player's average roll over the entire timeline.
+
+    Args:
+        The 'rolls' info from the Pandas dataframe created by 'pull_database_data()'
+
+    Return:
+        A Matplotlib barchart.
+    """
+    # Finding player names for the plot
+    connect = sqlite3.connect('TTRPG_rolls.db')
+    cursor = connect.cursor()
+    connect.execute("PRAGMA foreign_keys = ON")
+    cursor.execute("SELECT DISTINCT player_name FROM players")
+    names = []
+    all_items = cursor.fetchall()
+    for item in all_items:
+        names += item
+    connect.close()
+
+    # Grouping the data
+    group = data.groupby('player_id')['roll'].mean().reset_index()
+
+    # Plot the results
+    plt.bar(group['player_id'], group['roll'])
+    plt.xlabel('Players')
+    plt.xticks(group['player_id'], names)
+    plt.ylabel('d20 Dice Roll')
+    plt.title('Average Rolls By Player')
+    plt.show()
+
 def total_roll_average_by_game(data=pull_database_data('rolls')):
+    """Creates a barchart of the players' rolling average.
+
+    Args:
+        The 'rolls' info from the Pandas dataframe created by 'pull_database_data()'
+
+    Return:
+        A Matplotlib barchart.
+    """
     average_rolls = data.groupby('game_number')['roll'].mean().reset_index()
 
     # Plot the results
