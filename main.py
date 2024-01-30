2196ce725a8cd93183e589e6aa6d2ebb077746dd
StevenGaughran <73718182+StevenGaughran@users.noreply.github.com>
2024-01-30 14:04:58 -0800

    Renaming and main.py stuff
    Renaming and main.py stuff

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 import data_manipulation as dm
+import database_setup as ds
 
+# ~~~~~~~
+# Data Manipulation Functions
+# ~~~~~~~
 dm.all_players_roll_line_plot()
+dm.average_rolls_by_player()
+dm.total_roll_average_by_game()
+
+# ~~~~~~~
+# Database Setup
+# ~~~~~~~
+ds.csv_data_insertion(csv_data="ENTER WHAT YOU NEED", table="ENTER WHAT YOU NEED")
